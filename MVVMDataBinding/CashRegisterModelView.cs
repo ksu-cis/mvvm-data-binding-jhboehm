5d50ad727d2ced513157f599e07f7cf9c57f29dd
John Boehm <jhboehm@ksu.edu>
2020-04-03 20:38:25 -0500

    finished cash register

---
+++ MVVMDataBinding/CashRegisterModelView.cs
@@ -0,0 +1,57 @@
+﻿using CashRegister;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// The ModelView for a cash register control
+    /// </summary>
+    public class CashRegisterModelView : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event that notifies when properties change
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The Model class for this ModelView
+        /// </summary> 
+        CashDrawer drawer = new CashDrawer();
+
+        /// <summary>
+        /// The total current value of the drawer
+        /// </summary>
+        double TotalValue => drawer.TotalValue;
+
+        /// <summary>
+        /// Invokes the PropertyChanged event for denomination properties
+        /// and the TotalValue
+        /// </summary>
+        /// <param name="denomination">The property that is changing</param>
+        void InvokePropertyChanged(string denomination)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(denomination));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TotalValue"));
+        }
+
+        /// <summary>
+        /// Gets or sets the number of pennies in the cash register
+        /// </summary>
+        public int Pennies
+        {
+            get => drawer.Pennies;
+
+            set
+            {
+                if (drawer.Pennies == value || value < 0) return;
+                int quantity = value - drawer.Pennies;
+                if (quantity > 0) drawer.AddCoin(Coins.Penny, quantity);
+                else drawer.RemoveCoin(Coins.Penny, -quantity);
+                InvokePropertyChanged("Pennies");
+            }
+        }
+    }
+}
